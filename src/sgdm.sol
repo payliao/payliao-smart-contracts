a2d7580210e7e1c3f9b053b3e9f078bf2612a568
Xue Yao <9745649+xy-tech@users.noreply.github.com>
2024-01-20 18:10:26 +0800

    updated smart contracts to have access control

--- src/sgdm.sol
+++ src/sgdm.sol
@@ -4,10 +4,12 @@ pragma solidity 0.8.22;
 // import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 // import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface Iwhitelist {
 	/* 
